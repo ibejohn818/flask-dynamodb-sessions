a9106d96f9c12a2c3b8b288280813aec876a8e8d
John Hardy <john.hardy@me.com>
2019-06-22 10:58:36 -0700

    Bump version: 0.1.5 → 0.1.6

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ibejohn818/flask-dynamodb-sessions',
-    version='0.1.5',
+    version='0.1.6',
     zip_safe=False,
 )
